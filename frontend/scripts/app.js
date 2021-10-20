f179903d2fedf65af9de071818015df1061ff7cd
michaelianto <61343284+michaelianto@users.noreply.github.com>
2021-10-21 00:10:04 +0700

    Add files via upload

---
+++ frontend/scripts/app.js
@@ -0,0 +1,47 @@
+const sr = ScrollReveal();
+sr.reveal('.site-title', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 500
+});
+sr.reveal('.banner-img', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 750
+});
+sr.reveal('.about-img', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 500
+});
+sr.reveal('.about-title', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 750
+});
+sr.reveal('.skills-big-title', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 750
+});
+sr.reveal('.skills-container', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 1000
+});
+sr.reveal('.projects-title', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 750
+});
+sr.reveal('.card-project',{
+  delay: 750
+}, 100);
+sr.reveal('.experiences-title', {
+  origin: 'bottom',
+  distance: '50px',
+  delay: 750
+});
+sr.reveal('.card-experience',{
+  delay: 750
+}, 100);
